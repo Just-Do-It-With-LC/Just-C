f5d772ca7b6bc1d65f1736d539f6cbfae4010341
ahviplc <ahlc@sina.cn>
2019-09-25 11:01:42 +0800

    first commit.

---
+++ cmake-build-debug/CMakeFiles/Just_C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Just_C.dir/main.c.obj"
+  "Just_C.pdb"
+  "Just_C.exe"
+  "Just_C.exe.manifest"
+  "libJust_C.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Just_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Just-Do-It-With-LC!\n");
+    printf("Hello, World C!\n");
+    return 0;
+}
